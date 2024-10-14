8cc7722b97448aab155f3a7862f4fb8f7ffa376b
santosh <santosh@DESKTOP-68V5I16>
2024-10-14 19:29:37 +0530

    DemoFrameworkCretaed4

---
+++ DemoFramework/src/test/java/contact/ToCreateContactPage.java
@@ -0,0 +1,8 @@
+package contact;
+
+public class ToCreateContactPage {
+	public static void main(String [] args) {
+		System.out.println("create contact page");
+	}
+
+}
---
+++ DemoFramework/src/test/java/organization/ToCreateOrganizationPage.java
@@ -0,0 +1,10 @@
+package organization;
+
+public class ToCreateOrganizationPage {
+
+	public static void main(String[] args) {
+		System.out.println("organization page create");
+
+	}
+
+}
